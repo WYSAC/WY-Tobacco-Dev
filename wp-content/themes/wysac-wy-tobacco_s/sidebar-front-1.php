82ed9cd5ac63ce4019619f1c8e3f59db412bd2ac
jlschillinger <jessica.schillinger@uwyo.edu>
2015-08-17 16:07:37 -0600

    Front Page Sidebar 1
    Linked page for 1st front page sidebar

--- wp-content/themes/wysac-wy-tobacco_s/sidebar-front-1.php
+++ wp-content/themes/wysac-wy-tobacco_s/sidebar-front-1.php
@@ -4,4 +4,5 @@ Front Page Sidebar 1: Site Description next to slider
 
 <div id="front-sidebar-1">
   <?php dynamic_sidebar('front-sidebar-1'); ?>
+  <?php $id = 82; $p = get_page($id); echo apply_filters('the_content', $p->post_content); ?>
 </div>
