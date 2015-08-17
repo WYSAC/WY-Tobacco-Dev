e6afe797105918b34e48b2963a03ec0e6f2c7f20
jlschillinger <jessica.schillinger@uwyo.edu>
2015-08-17 16:03:51 -0600

    Front Page Widgets
    Installed script to show specific pages in sidebar areas.  Began to
    format the content within the front page sidebar areas.

--- wp-content/themes/wysac-wy-tobacco_s/sidebar-front-2.php
+++ wp-content/themes/wysac-wy-tobacco_s/sidebar-front-2.php
@@ -4,4 +4,5 @@ Front Page Sidebar 1: Site Description next to slider
 
 <div id="front-sidebar-2">
   <?php dynamic_sidebar('front-sidebar-2'); ?>
+  <?php $id = 73; $p = get_page($id); echo apply_filters('the_content', $p->post_content); ?>
 </div>
