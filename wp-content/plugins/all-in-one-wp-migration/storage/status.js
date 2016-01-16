3962136d8d7a6d35ff826a3a5819f0da1b8f92a1
jlschillinger <jessica.schillinger@uwyo.edu>
2016-01-15 17:00:47 -0700

    installed plug-in to move to dev server

---
+++ wp-content/plugins/all-in-one-wp-migration/storage/status.js
@@ -0,0 +1,1 @@
+{"type":"download","message":"<a href=\"http:\/\/www.wytobacco.dev\/wp-content\/ai1wm-backups\/www.wytobacco.dev-20160115-174504-799.wpress\" class=\"ai1wm-button-green ai1wm-emphasize\"><span>Download www.wytobacco.dev<\/span><em>Size: 111 MB<\/em><\/a>","processed":2318,"total":2318}
