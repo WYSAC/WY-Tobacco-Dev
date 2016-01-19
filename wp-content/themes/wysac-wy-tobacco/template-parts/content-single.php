bc10b280c5c07200f0b4fe7e5dd479d19809752c
jlschillinger <jessica.schillinger@uwyo.edu>
2016-01-19 12:17:54 -0700

    Add template part for Co-Authors Plus plug-in

--- wp-content/themes/wysac-wy-tobacco/template-parts/content-single.php
+++ wp-content/themes/wysac-wy-tobacco/template-parts/content-single.php
@@ -30,7 +30,41 @@
 						?></h2>
 
 		<div class="entry-meta">
-			<?php wysac_wy_tobacco_posted_on();?>
+			<? php if ( ! function_exists( 'twentyten_posted_on' ) ) :
+							/**
+							 * Integrate Co-Authors Plus with wysac-wy-tobacco by replacing wysac_wy_tobacco_posted_on() with this function
+							 */
+							function wysac_wy_tobacco_posted_on() {
+							    if ( function_exists( 'coauthors_posts_links' ) ) :
+							        printf( __( '<span class="%1$s">Posted on</span> %2$s <span class="meta-sep">by</span> %3$s', 'wysac_wy_tobacco' ),
+							            'meta-prep meta-prep-author',
+							            sprintf( '<a href="%1$s" title="%2$s" rel="bookmark"><span class="entry-date">%3$s</span></a>',
+							                get_permalink(),
+							                esc_attr( get_the_time() ),
+							                get_the_date()
+							            ),
+							            coauthors_posts_links( null, null, null, null, false )
+							        );
+							    else:
+							        printf( __( '<span class="%1$s">Posted on</span> %2$s <span class="meta-sep">by</span> %3$s', 'wysac_wy_tobacco' ),
+							            'meta-prep meta-prep-author',
+							            sprintf( '<a href="%1$s" title="%2$s" rel="bookmark"><span class="entry-date">%3$s</span></a>',
+							                get_permalink(),
+							                esc_attr( get_the_time() ),
+							                get_the_date()
+							            ),
+							            sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s">%3$s</a></span>',
+							                get_author_posts_url( get_the_author_meta( 'ID' ) ),
+							                esc_attr( sprintf( __( 'View all posts by %s', 'wysac_wy_tobacco' ), get_the_author() ) ),
+							                get_the_author()
+							            )
+							        );
+							    endif;
+							}
+							endif;
+							?>
+
+								<?php /* wysac_wy_tobacco_posted_on();*/ ?>
 		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
